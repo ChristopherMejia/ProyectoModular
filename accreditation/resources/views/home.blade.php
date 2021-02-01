d6e28a0e6e8117a8d8a9e35090852715c7d7a139
yvesgarciaespinosa <yvesgarciaespinosa@gmail.com>
2021-02-01 12:45:46 -0600

    Forma base de la vista home, creada tabla de organismos

--- accreditation/resources/views/home.blade.php
+++ accreditation/resources/views/home.blade.php
@@ -1,20 +1,27 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+        <a href="plantilla/create"><button>Crear nueva plantilla</button></a>
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
+                <div class="card-header">Plantillas</div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                <table class="table table-striped table-bordered table-condensed table-hover">
+                    <thead>
+                        <th>Organismo</th>
+                        <th>Versión</th>
+                        <th colspan=2>Acciones</th>
+                    </thead>
+                        @foreach($plantillas as $plantilla)
+                                <tr>
+                                    <td>{{$plantilla->organismo}}</td>
+                                    <td>{{$plantilla->version}}</td>
+                                    <td><button>Modificar</button></td>
+                                    <td><button>Eliminar</button></td>
+                                </tr>
+                        @endforeach
+                    </table>
                 </div>
             </div>
         </div>
