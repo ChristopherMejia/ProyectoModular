ccb77e6ba646071e8e578523b498aab0a90239d5
Christopher Mejia <chis.mejia.garcia@gmail.com>
2021-02-04 20:21:19 -0600

    Mostramos los organismos en la vista

--- accreditation/resources/views/home.blade.php
+++ accreditation/resources/views/home.blade.php
@@ -1,30 +1,28 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-        <a href="plantilla/create"><button>Crear nueva plantilla</button></a>
-            <div class="card">
-                <div class="card-header">Plantillas</div>
-                <div class="card-body">
-                <table class="table table-striped table-bordered table-condensed table-hover">
-                    <thead>
-                        <th>Organismo</th>
-                        <th>Versión</th>
-                        <th colspan=2>Acciones</th>
-                    </thead>
-                        @foreach($plantillas as $plantilla)
-                                <tr>
-                                    <td>{{$plantilla->organismo}}</td>
-                                    <td>{{$plantilla->version}}</td>
-                                    <td><button>Modificar</button></td>
-                                    <td><button>Eliminar</button></td>
-                                </tr>
-                        @endforeach
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
+
+
+<div class="container-home">
+    <div class="home-header">Organismos</div>
+    <table class="table table-hover">
+        <thead>
+            <th>Id</th>
+            <th>Nombre</th>
+            <th>Acciones</th>
+        </thead>
+        @foreach ($Organismos as $organismo)
+            <tr>
+                <td id='id-organismo'>{{$organismo->id}}</td>
+                <td id='name-organismo'>{{$organismo->nombre}}</td>
+                <td>
+                    <a class=" btn btn-outline-primary" href="/home/{{$organismo->id}}/edit">Edit</a>
+                    <a class="btn btn-outline-danger" href="/home/{{$organismo->id}}/confirmDelete">Delete</a>
+                </td>
+            </tr>
+        @endforeach
+
+    </table>
+
 </div>
+
 @endsection
