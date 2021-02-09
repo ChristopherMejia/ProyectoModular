d0612c3d1d7f938ca3aa03ce98689f0c22b9a67d
Christopher Mejia <chis.mejia.garcia@gmail.com>
2021-02-08 21:58:55 -0600

    modificamos el app.blade

--- accreditation/resources/views/layouts/app.blade.php
+++ accreditation/resources/views/layouts/app.blade.php
@@ -17,11 +17,13 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/home.css') }}" rel="stylesheet">
+    <script src="{{ asset('css/sidebar.css') }}" defer></script>
+
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
+            <div class="container" style="margin: 0"> 
                 <a class="navbar-brand" href="{{ url('/home') }}">
                     {{ config('app.name', 'Accreditation') }}
                 </a>
@@ -38,19 +40,9 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
-                        @guest
-                            <!-- <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li> -->
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ Auth::user()->first_name}} {{Auth::user()->last_name}}  <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
@@ -60,20 +52,23 @@
                                         {{ __('Logout') }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    <form id="logout-form" action="{{ route('logout') }}" method="GET" style="display: none;">
                                         @csrf
                                     </form>
                                 </div>
                             </li>
-                        @endguest
+                                                        
                     </ul>
                 </div>
             </div>
         </nav>
 
+        @include('partials\sidebar')
+        
         <main class="py-4">
             @yield('content')
         </main>
+
     </div>
 </body>
 </html>
