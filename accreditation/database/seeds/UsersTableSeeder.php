7ad2f08e7c7b947dc7c14a4d1bd6e8368189ba40
Christopher Mejia <chis.mejia.garcia@gmail.com>
2021-02-08 22:00:39 -0600

    creamos nuestro UsesrsTableSeeder

---
+++ accreditation/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\User;
+
+
+class UsersTableSeeder extends Seeder
+{
+
+    public function run()
+    {
+        DB::table('users')->delete();
+    
+        User::create(array(
+            'first_name'     => 'Christopher',
+            'last_name' => 'Mejia',
+            'email'    => 'chris@gmail.com',
+            'password' => Hash::make('awesome'),
+        ));
+    }
+
+}
