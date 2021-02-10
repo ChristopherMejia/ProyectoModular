c048c59dc4a4021317840f1fe8a6da2289abaa55
Christopher Mejia <chis.mejia.garcia@gmail.com>
2021-02-09 21:55:27 -0600

    modificamos la función login

--- accreditation/app/Http/Controllers/AuthController.php
+++ accreditation/app/Http/Controllers/AuthController.php
@@ -13,36 +13,36 @@ class AuthController extends Controller
     }
     public function login()
     {
-        $rules = array(
-            'email'    => 'required|email', // make sure the email is an actual email
-            'password' => 'required|alphaNum|min:3' // password can only be alphanumeric and has to be greater than 3 characters
-        );
+        // $rules = array(
+        //     'email'    => 'required|email', // make sure the email is an actual email
+        //     'password' => 'required|alphaNum|min:3' // password can only be alphanumeric and has to be greater than 3 characters
+        // );
 
-        $validator = Validator::make(Input::all(), $rules);
+        // $validator = Validator::make(Input::all(), $rules);
 
-        // if the validator fails, redirect back to the form
-        if ($validator->fails()) {
-            return Redirect::to('login');
+        // // if the validator fails, redirect back to the form
+        // if ($validator->fails()) {
+        //     return Redirect::to('login');
                 
-        } else {
+        // } else {
         
-            // create our user data for the authentication
-            $userdata = array(
-                'email'     => Input::get('email'),
-                'password'  => Input::get('password')
-            );
+        //     // create our user data for the authentication
+        //     $userdata = array(
+        //         'email'     => Input::get('email'),
+        //         'password'  => Input::get('password')
+        //     );
         
-            // attempt to do the login
-            if (Auth::attempt($userdata)) {
-                echo 'SUCCESS!';
-            } else {        
+        //     // attempt to do the login
+        //     if (Auth::attempt($userdata)) {
+        //         echo 'SUCCESS!';
+        //     } else {        
             
-                // validation not successful, send back to form 
-                return redirect::to('login');
+        //         // validation not successful, send back to form 
+        //         return redirect::to('login');
             
-            }
+        //     }
         
-        }
+        // }
     }
 
     public function logout()
