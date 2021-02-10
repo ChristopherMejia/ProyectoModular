b30925a13580c6a0373e888b43885f015a18b933
Christopher Mejia <chis.mejia.garcia@gmail.com>
2021-02-09 21:57:44 -0600

    agregando estilos y icons

--- accreditation/resources/views/partials/sidebar.blade.php
+++ accreditation/resources/views/partials/sidebar.blade.php
@@ -1,25 +1,17 @@
 <div class="sidebar">
-    <nav class="sidebar-nav" style="padding-top: 50px; ">
-      <ul class="nav">
-        <li class="nav-item">
-          <a class="nav-link" href="home">
-            <i class="nav-icon icon-speedometer"></i> Dashboard
-          </a>
-        </li>
-        <li class="nav-title">Módulos</li>
-          <li class="nav-item">
-                <a class="nav-link" href="home/create">
-                  <i class="nav-icon icon-doc"></i> Crear Usuario</a>
-              </li> 
-          </li>
-          <li class="nav-item">
-          <a class="nav-link" href="home/usuarios">
-                  <i class="nav-icon icon-docs"></i> Desplegar Usuarios</a>
-              </li> 
-          </li> 
-        </ul>
-    </nav> 
-  </div>
+	<nav>
+		<ul>
+			<li>
+				<span class="material-icons">accessibility_new</span>
+				<a href="">Usuario</a>
+			</li>
+			<li>
+				<span class="material-icons">text_snippet</span>
+				<a  href="">Plantillas</a>
+			</li>
+		</ul>
+	</nav>
+</div>
 
 
 
