a7ba5db2bb63de1edec5916f788eb1d924b371ee
Christopher Mejia <chis.mejia.garcia@gmail.com>
2021-02-10 20:28:37 -0600

    Agregando archivo js

--- accreditation/resources/views/home.blade.php
+++ accreditation/resources/views/home.blade.php
@@ -1,36 +1,8 @@
 @extends('layouts.app')
 @section('content')
-<<<<<<< HEAD
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-        <a href="create"><button>Crear nueva plantilla</button></a>
-            <div class="card">
-                <div class="card-header">Plantillas</div>
-                <div class="card-body">
-                <table class="table table-striped table-bordered table-condensed table-hover">
-                    <thead>
-                        <th>Organismo</th>
-                        <th>Versión</th>
-                        <th colspan=2>Acciones</th>
-                    </thead>
-                        @foreach($plantillas as $plantilla)
-                                <tr>
-                                    <td>{{$plantilla->organismo}}</td>
-                                    <td>{{$plantilla->version}}</td>
-                                    <td><button>Modificar</button></td>
-                                    <td><button>Eliminar</button></td>
-                                </tr>
-                        @endforeach
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
-=======
 
 <!-- <div class="container-home">
-    <div class="home-header">Organismos</div>
+<div class="home-header">Organismos</div>
     <table class="table table-hover">
         <thead class="table-dark">
             <th>Id</th>
@@ -48,12 +20,6 @@
             </tr>
         @endforeach
     </table>
-<<<<<<< HEAD
 </div> -->
-=======
-
->>>>>>> 6e0fec52ac542e05e9cfb77d6473f50d2ba0beaf
-</div>
->>>>>>> 06fd78d3174e2ebf5677674f11515e9f0d0eaaea
 
 @endsection
