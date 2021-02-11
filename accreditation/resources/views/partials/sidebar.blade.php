f9939d44a5bb5e0d0a805c7e34ccf20998b9045e
Christopher Mejia <chis.mejia.garcia@gmail.com>
2021-02-10 20:31:04 -0600

    Ajustando detalles en el html

--- accreditation/resources/views/partials/sidebar.blade.php
+++ accreditation/resources/views/partials/sidebar.blade.php
@@ -1,17 +1,32 @@
 <div class="sidebar">
 	<nav>
-		<ul>
+		<ul class="list-unstyled components">
 			<li>
-				<span class="material-icons">accessibility_new</span>
-				<a href="">Usuario</a>
+				<span class="material-icons icon">accessibility_new</span>
+				<a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle ">Usuario</a>
+				<ul class="collapse list-unstyled" id="homeSubmenu">
+					<li>
+                        <a href="#">Desplegar Usuarios</a>
+                    </li>
+                    <li>
+                        <a href="#">Crear Usuario</a>
+                    </li>
+				</ul>
 			</li>
 			<li>
 				<span class="material-icons">text_snippet</span>
-				<a  href="">Plantillas</a>
+				<a href="#PlantillaSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle ">Plantilla</a>
+				<ul class="collapse list-unstyled" id="PlantillaSubmenu">
+					<li>
+                        <a href="#">Crear Plantilla</a>
+                    </li>
+                    <li>
+                        <a href="#">Mostrar Plantilla</a>
+                    </li>
+				</ul>
 			</li>
 		</ul>
 	</nav>
 </div>
-
 
 
