8c351c79857dc012f30b7823c416d3f325ecb8bb
Christopher Mejia <chis.mejia.garcia@gmail.com>
2021-02-10 22:21:24 -0600

    removiendo codigo comentado

--- accreditation/resources/views/partials/sidebar.blade.php
+++ accreditation/resources/views/partials/sidebar.blade.php
@@ -25,6 +25,18 @@
                     </li>
 				</ul>
 			</li>
+			<li>
+				<span class="material-icons">text_snippet</span>
+				<a href="#OrgSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle ">Organismo</a>
+				<ul class="collapse list-unstyled" id="OrgSubmenu">
+					<li>
+                        <a href="#">Crear Organismo</a>
+                    </li>
+                    <li>
+                        <a href="#">Mostrar Organimos</a>
+                    </li>
+				</ul>
+			</li>
 		</ul>
 	</nav>
 </div>
