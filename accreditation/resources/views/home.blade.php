5ac74f5d7209cd02568b873ba7b6dd25d7072010
Christopher Mejia <chis.mejia.garcia@gmail.com>
2021-02-16 22:44:46 -0600

    modificamos el template

--- accreditation/resources/views/home.blade.php
+++ accreditation/resources/views/home.blade.php
@@ -1,25 +1,4 @@
 @extends('layouts.app')
 @section('content')
 
-<!-- <div class="container-home">
-<div class="home-header">Organismos</div>
-    <table class="table table-hover">
-        <thead class="table-dark">
-            <th>Id</th>
-            <th>Nombre</th>
-            <th>Acciones</th>
-        </thead>
-        @foreach ($Organismos as $organismo)
-            <tr>
-                <td id='id-organismo'>{{$organismo->id}}</td>
-                <td id='name-organismo'>{{$organismo->nombre}}</td>
-                <td>
-                    <a class=" btn btn-outline-primary" href="/home/{{$organismo->id}}/edit">Edit</a>
-                    <a class="btn btn-outline-danger" href="/home/{{$organismo->id}}/confirmDelete">Delete</a>
-                </td>
-            </tr>
-        @endforeach
-    </table>
-</div> -->
-
 @endsection
