cc95f239cccd4945c72d0461f680e47549837d59
Christopher Mejia <chis.mejia.garcia@gmail.com>
2021-02-16 22:45:30 -0600

    creamos el template para mostrar organismos

--- accreditation/resources/views/organismo/show.blade.php
+++ accreditation/resources/views/organismo/show.blade.php
@@ -1,21 +1,28 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-            <label for="Organismo">Organismo</label>
-            <select name="Organismo" class="selectpicker">
-            @foreach($organismos as $orgs)
-						<option value="{{$orgs->nombre}}">{{$orgs->nombre}}</option>
-			@endforeach
-            </select>
-            <label for="Organismo">Versión</label>
-            <input type="text" name="Version">
-            </input>
-            <button type="button" id="bt_crear" class="btn btn-primary">Crear</button>
-            </div>
-        </div>
-    </div>
+<div class="main-container">
+
+    <table class="table table-dark table-striped">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Acciones</th>
+            </tr>
+        </thead>
+        @foreach($Organismos as $organismo)
+        <tbody>
+            <tr>
+                <td scope="row">{{$organismo->id}}</td>
+                <td scope="row">{{$organismo->nombre}}</td>
+                <td>
+                <a href=""><span class="material-icons">mode_edit</span></a>
+                <a href=""><span class="material-icons">delete_outline</span></a>
+                </td>
+            </tr>
+        </tbody>
+        @endforeach
+    </table>
+           
 </div>
 @endsection
