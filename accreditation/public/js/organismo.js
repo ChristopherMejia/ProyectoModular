a8bc504ccaa9ad58b8f900ad59d2e0533b0646e1
Christopher Mejia <chis.mejia.garcia@gmail.com>
2021-02-18 18:51:33 -0600

    codigo js, para organimos

---
+++ accreditation/public/js/organismo.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+
+    const edit_item = document.getElementById("edit-item");
+    const edit_modal = document.getElementById("edit-modal");
+    
+    edit_item.addEventListener('shown.bs.modal', function (){
+        
+    });
+});
+
+
+
