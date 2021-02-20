2cac452d983aee91bb60534cd00ddb7ff2dc9e35
Christopher Mejia <chis.mejia.garcia@gmail.com>
2021-02-19 23:14:55 -0600

    agregabdo al DOM el modal

--- accreditation/resources/views/organismo/show.blade.php
+++ accreditation/resources/views/organismo/show.blade.php
@@ -2,54 +2,57 @@
 @section('content')
 <div class="main-container">
 
-    <table class="table table-dark table-striped">
-        <thead>
+    <table class="table table-hover table-sm">
+        <thead class=" table-light">
             <tr>
                 <th scope="col">#</th>
                 <th scope="col">Nombre</th>
                 <th scope="col">Acciones</th>
             </tr>
         </thead>
-        @foreach($Organismos as $organismo)
         <tbody>
+        @foreach($Organismos as $organismo)
             <tr>
                 <td id="organismoId" scope="row">{{$organismo->id}}</td>
                 <td id="organismoNombre"scope="row">{{$organismo->nombre}}</td>
                 <td>
-                <button id ="edit-item" type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#editModal">
-                    <span class="material-icons">mode_edit</span>
+                <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#edit-item" id="btnEdit">
+                    <span class="material-icons" data-id="{{$organismo->id}}" data-name="{{$organismo->nombre}}">mode_edit</span>
                 </button>
-                <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#deleteModal">
+                <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
                     <span class="material-icons">delete_outline</span>
                 </button>
                 </td>
             </tr>
-        </tbody>
         @endforeach
+        </tbody>
     </table>
 
-<!-- Modal -->
-<div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
+  <!-- Modal Edit -->
+  <div class="modal fade" id="edit-item" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="staticBackdropLabel">Editar Organismo</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+        <input id="nameToEdit" type="text" class="form-control" disabled>
+          <form action="/organismo/edit" method="post">
+          @csrf
             <div class="form-group">
-              <label for="modal-input-name">Nombre</label>
-              <input type="text" class="form-control" id="modal-input-name" name="organismoName" required>
-              <input type="hidden" id="modal-input-id" name="organismo_id">
+              <input type="text" name="name" class="form-control" placeholder="Ingrese Nombre">
+              <input type="hidden" name="id" id="editId">
             </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+          <button type="button" class="btn btn-primary">Guardar</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
            
 </div>
 @endsection
