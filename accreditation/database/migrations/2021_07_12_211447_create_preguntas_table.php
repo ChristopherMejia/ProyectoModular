41743209b7e08857ba7e2f08fa74311390afdc12
yvesgarciaespinosa <yvesgarciaespinosa@gmail.com>
2021-07-12 17:25:01 -0500

    Creadas tablas de preguntas, subcategorias, y categorias

---
+++ accreditation/database/migrations/2021_07_12_211447_create_preguntas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePreguntasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('preguntas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('idTipo');
+            $table->string('descripcion');
+            $table->boolean('conEvidencia');
+            $table->boolean('conAdjunto');
+            $table->unsignedBigInteger('idSubcategoria');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('preguntas');
+    }
+}
---
+++ accreditation/database/migrations/2021_07_12_213310_create_subcategorias_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubcategoriasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subcategorias', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('idCategoria');
+            $table->string('descripcion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subcategorias');
+    }
+}
