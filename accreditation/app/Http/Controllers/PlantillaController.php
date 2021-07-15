d405c61da66502c5c882ee1c12357c52a1b86d36
Christopher Mejia <chis.mejia.garcia@gmail.com>
2021-07-14 23:55:59 -0500

    trabajando en la creación de usuarios

--- accreditation/app/Http/Controllers/PlantillaController.php
+++ accreditation/app/Http/Controllers/PlantillaController.php
@@ -10,10 +10,10 @@ use DB;
 
 class PlantillaController extends Controller
 {
-    /*public function __construct()
+    public function __construct()
     {
         $this->middleware('auth');
-    }*/
+    }
 
     public function index()
     {
