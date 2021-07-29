9d5be78251b7368753be02ce0e95202c9418b4ec
yvesgarciaespinosa <yvesgarciaespinosa@gmail.com>
2021-07-29 16:32:34 -0500

    Agregada tabla de subpreguntas

---
+++ accreditation/app/Subpregunta.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Subpregunta extends Model
+{
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+    protected $fillable =[
+        'idTipo',
+        'descripcion',
+        'conEvidencia',
+        'conAdjunto',
+        'idSubcategoria',
+        'identificacion',
+    ];
+}
